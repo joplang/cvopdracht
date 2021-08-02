a5a8242425788f6a09da558e83dd665bd15f7eb1
Toby Versteeg <toby@codegorilla.nl>
2021-08-02 16:59:59 +0200

    Relations in education table

--- app/Database/Migrations/006_educations_table.php
+++ app/Database/Migrations/006_educations_table.php
@@ -12,7 +12,12 @@ return [
         `end_year` year DEFAULT NULL,
         `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
         `info` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
-        `deleted` datetime DEFAULT NULL,
+        `created` timestamp NOT NULL,
+        `updated` timestamp DEFAULT CURRENT_TIMESTAMP,
+        `deleted` timestamp DEFAULT NULL,
+        `created_by` int(11) NOT NULL,
+        `updated_by` int(11),
+        `deleted_by` int(11),
         PRIMARY KEY (`id`)
       ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;",
 
@@ -21,7 +26,6 @@ return [
         'ALTER TABLE `users` ADD FOREIGN KEY (`created_by`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;',
         'ALTER TABLE `users` ADD FOREIGN KEY (`updated_by`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;',
         'ALTER TABLE `users` ADD FOREIGN KEY (`deleted_by`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;',
-
     ],
 
     'seeder' => [
